c35523abb3437b66df0d60ecef774ef8e2cfda37
Troels Ugilt Jensen <tujensen@gmail.com>
2018-08-07 20:38:18 +0200

    ER-36: Added modify of screen result by campaigns

--- Service/MiddlewareService.php
+++ Service/MiddlewareService.php
@@ -27,25 +27,77 @@ class MiddlewareService extends BaseService
                 ':now between campaign.scheduleFrom and campaign.scheduleTo'
             )
             ->andWhere(
-                ':screen member of campaign.screens'
+                $queryBuilder->expr()->orX(
+                    ':screen member of campaign.screens',
+                    ':groups member of campaign.screenGroups'
+                )
             )
             ->setParameter('screen', $screen)
+            ->setParameter('groups', $screen->getGroups())
             ->setParameter('now', $now)
             ->getQuery()->getResult();
 
         return $campaigns;
     }
 
-    public function getCurrentScreenArray($screenId) {
-        $screen = $this->entityManager->getRepository(Screen::class)->findOneById($screenId);
+    /**
+     * Get the current screen array.
+     *
+     * @param $screenId
+     * @return object
+     */
+    public function getCurrentScreenArray($screenId)
+    {
+        // Get regular results.
+        $result = parent::getCurrentScreenArray($screenId);
 
+        $screen = $this->entityManager->getRepository(Screen::class)
+            ->findOneById($screenId);
+
+        // Modify results with campaigns.
         $campaigns = $this->getCampaignsForScreen($screen);
 
-        // If no campaigns apply, run normal code.
-        if (count($campaigns) === 0) {
-            return parent::getCurrentScreenArray($screenId);
+        if (count($campaigns) > 0) {
+            // Remove all channels from region 1.
+            foreach ($result->channels as $key => $aChannel) {
+                $regions = [];
+
+                foreach ($aChannel->regions as $region) {
+                    if ($region != 1) {
+                        $regions[] = $region;
+                    }
+                }
+
+                if (count($regions) == 0) {
+                    unset($result->channels[$key]);
+                }
+            }
+
+            // Add all campaign channels to region 1.
+            foreach ($campaigns as $campaign) {
+                $channels = $campaign->getChannels();
+
+                foreach ($channels as $channel) {
+                    $channelId = $channel->getId();
+                    $data = $this->getChannelArray($channel);
+
+                    if (!isset($result->channels[$channelId])) {
+                        $result->channels[$channelId] = (object)[
+                            'data' => $data->data,
+                            'regions' => [1],
+                        ];
+                    }
+                    else {
+                        $regions = $result->channels[$channelId]->regions;
+
+                        if (!in_array(1, $regions)) {
+                            $result->channels[$channelId]->regions[] = 1;
+                        }
+                    }
+                }
+            }
         }
 
-
+        return $result;
     }
 }
